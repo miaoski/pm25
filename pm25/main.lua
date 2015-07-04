b7eeec0449a8743960911c14548133dc263ebdb0
Lin Zhemin <lin.zhemin@gmail.com>
2015-07-04 22:30:28 +0800

    Send values to server

--- pm25/main.lua
+++ pm25/main.lua
@@ -1,2 +1,18 @@
-print "main.lua"
+print ">>> main.lua"
+http = require("http")
 
+host = "220.135.254.63"	-- miaoski.idv.tw
+port = 2080
+
+hum = 0		-- Humidity
+tmp = 0		-- Temperature
+mq9 = 0		-- MQ9 voltage
+pm25 = 0	-- DN7C3CA006 voltage
+
+-- Send URL every 5s
+tmr.alarm(0, 5000, 1, function()
+	url = "/hum/" .. hum .. "/temp/" .. tmp .. "/mq9/" .. mq9 .. "/dn7/" .. pm25
+	print("GET http://" .. host .. ":" .. port .. url)
+	http.get(host, port, url, {}, function(conn)
+	end)
+end)
