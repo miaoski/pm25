f2d9ab74ed226702ec936d0c4dc9def63b39c4be
Lin Zhemin <lin.zhemin@gmail.com>
2015-07-04 21:45:03 +0800

    Show number of trial

--- pm25/init.lua
+++ pm25/init.lua
@@ -18,21 +18,21 @@ if(gpio.read(pin) == 1 and file.open("config.lua")) then
 
 	cnt = 0
 	tmr.alarm(0, 1000, 1, function()
-  		if cnt < max_retry and wifi.sta.getip() then
+  		if(cnt <= max_retry and wifi.sta.getip()) then
     			tmr.stop(0)
     			print("IP:  " .. wifi.sta.getip())
     			print("MAC: " .. wifi.sta.getmac())
     			dofile("main.lua")
   		else
-			if cnt < max_retry then
-				print("Getting an IP...")
+			cnt = cnt + 1
+			if cnt <= max_retry then
+				print("Getting IP ... #" .. cnt)
 			else
 				tmr.stop(0)
 				print("Unable to get IP.  Enter AP mode.")
 				dofile("ap.lua")
 			end
   		end
-		cnt = cnt + 1
 	end)
 else
 	if(gpio.read(pin) == 0) then
