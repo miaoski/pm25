a1367ac367004e572b03238f699d7911d84456b5
Lin Zhemin <lin.zhemin@gmail.com>
2015-07-05 14:25:33 +0800

    check params and fails silently

--- server/ar.py
+++ server/ar.py
@@ -1,18 +1,27 @@
+# -*- coding: utf8 -*-
 from flask import Flask
 import uuid
+import re
 
 app = Flask(__name__)
 
+re_uuid = re.compile(r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}')
+
 @app.route('/id/<uuid>/hum/<hum>/tmp/<tmp>/mq9/<mq9>/dn7/<dn7>')
 def submit_value(uuid, hum, tmp, mq9, dn7):
-	hum = float(hum)
-	tmp = float(tmp)
-	mq9 = float(mq9)
-	dn7 = float(dn7)
+	"不論如何都回應 OK, 200 免得有人來 try UUID"
+	try:
+		hum = float(hum)
+		tmp = float(tmp)
+		mq9 = float(mq9)
+		dn7 = float(dn7)
+		assert(re_uuid.match(uuid) is not None)
+	except:
+		return 'OK', 200
 	print "Got params", uuid, hum, tmp, mq9, dn7
-	if len(uuid) != 36:
-		print 'Invalid UUID!'
+	print "Save to DB"
 	return 'OK', 200
+
 
 @app.route('/register')
 def register():
@@ -21,5 +30,5 @@ def register():
 
 if __name__ == "__main__":
 	app.config['JSON_AS_ASCII'] = False     # JSON in UTF-8
-	app.config['DEBUG'] = True
+	app.config['DEBUG'] = False
 	app.run(host = '0.0.0.0', threaded=True, port=2080)
